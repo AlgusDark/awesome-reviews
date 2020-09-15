7ac447ad5f5d3502e2e4b2260d5fcb2f5de5186c
Carlos Perez Gutierrez <carlos@medmain.com>
2020-09-15 21:01:43 +0900

    Add configuration

---
+++ next-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+
+declare module "*.svg" {
+  import * as React from "react";
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
